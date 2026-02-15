4307626c9c9261c5935ca65a1717184287fa54a7
hannah-albi <hannah.albi@ontariotechu.net>
2026-02-14 22:33:43 -0500

    Update players.c

--- players.c
+++ players.c
@@ -1,7 +1,7 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U: Operating Systems
  *
- * Copyright (C) 2026, 6
+ * Copyright (C) 2026, <6>
  * All rights reserved.
  *
  */
@@ -13,12 +13,24 @@
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
 {
-    return false;
+   for(int i = 0; i < num_players; i++){
+        if(strcmp(name, players[i].name) == 0) //Comparing the current
+                                               //player name with each existing
+                                               //player name with strcmp
+                return true; //true if their name already exists
+   }
+   return false; //false if the player's name does not exist
 }
 
-// Go through the list of players and update the score for the 
+// Go through the list of players and update the score for the
 // player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
-    
+  for(int i = 0; i < num_players; i++){
+        if(strcmp(name, players[i].name) == 0)
+                players[i].score += score; //if the player's name exists
+                                           //their score will be updated
+                break; //break is used so that the loop stops after the first
+                       //match and update
+  }
 }
