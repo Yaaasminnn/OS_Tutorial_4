f9c3fdbec118894d0242d1a4bdeaa8080f51501e
Steven-Sunny <sunsteven796@gmail.com>
2026-02-15 00:45:23 -0500

    formatting

--- players.c
+++ players.c
@@ -13,24 +13,26 @@
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
 {
-   for(int i = 0; i < num_players; i++){
-        if(strcmp(name, players[i].name) == 0) //Comparing the current
-                                               //player name with each existing
-                                               //player name with strcmp
-                return true; //true if their name already exists
-   }
-   return false; //false if the player's name does not exist
+  for (int i = 0; i < num_players; i++)
+  {
+    if (strcmp(name, players[i].name) == 0) // Comparing the current
+                                            // player name with each existing
+                                            // player name with strcmp
+      return true; // true if their name already exists
+  }
+  return false; // false if the player's name does not exist
 }
 
 // Go through the list of players and update the score for the
 // player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
-  for(int i = 0; i < num_players; i++){
-        if(strcmp(name, players[i].name) == 0)
-                players[i].score += score; //if the player's name exists
-                                           //their score will be updated
-                break; //break is used so that the loop stops after the first
-                       //match and update
+  for (int i = 0; i < num_players; i++)
+  {
+    if (strcmp(name, players[i].name) == 0)
+      players[i].score += score; // if the player's name exists
+                                 // their score will be updated
+    break; // break is used so that the loop stops after the first
+           // match and update
   }
 }
